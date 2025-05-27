3dfd19213528639b09df0baacde6b071204da953
Judit Kisistok <judit.kisistok@gmail.com>
2025-05-27 18:27:26 +0200

    orchestration - turning jupyter notebook into python script

---
+++ 03-orchestration/duration-prediction.py
@@ -0,0 +1,127 @@
+import argparse
+import pickle
+
+from pathlib import Path
+
+import mlflow
+import polars as pl
+import xgboost as xgb
+
+from sklearn.metrics import root_mean_squared_error
+from sklearn.feature_extraction import DictVectorizer
+from mlflow.models.signature import infer_signature
+
+models_folder = Path("models")
+models_folder.mkdir(exist_ok=True)
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+def read_dataframe(year, month, color = "green"): 
+
+    url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02d}.parquet"
+    
+    df = (
+        pl.read_parquet(url)
+        .with_columns(
+            (pl.col("lpep_dropoff_datetime") - pl.col("lpep_pickup_datetime"))
+            .dt.total_seconds()
+            .alias("duration")
+        )
+        .with_columns(
+            (pl.col("duration") / 60)
+            .alias("duration_minutes")
+        )
+        .with_columns(
+            pl.concat_str(pl.col("PULocationID"), pl.lit("_"), pl.col("DOLocationID")).alias("PU_DO")
+        )
+        .filter((pl.col("duration_minutes") > 1) & (pl.col("duration_minutes") <= 60))
+        .with_columns(pl.col(["PULocationID", "DOLocationID"]).cast(pl.Utf8))
+    )
+
+    return df
+
+def create_X(df, dv = None):
+    categorical = ["PU_DO"]
+    numerical = ["trip_distance"]
+    dicts = df.select(categorical + numerical).to_dicts()
+
+    if dv is None:
+        dv = DictVectorizer(sparse = True)
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+
+    return X, dv
+
+def train_model(X_train, y_train, X_val, y_val, dv):
+    with mlflow.start_run() as run:
+        train = xgb.DMatrix(X_train, label=y_train)
+        valid = xgb.DMatrix(X_val, label=y_val)
+
+        params = {
+            "reg_lambda": 0.10479357768633932,
+            "seed": 42,
+            "max_depth": 67,
+            "min_child_weight": 4.726134327631799,
+            "learning_rate": 0.2254998660071301,
+            "objective": "reg:squarederror",
+            "reg_alpha": 0.012770048180595262,
+        }
+
+        mlflow.log_params(params)
+
+        booster = xgb.train(
+                params=params,
+                dtrain=train,
+                num_boost_round=30,
+                evals=[(valid, "validation")],
+                early_stopping_rounds=50,
+            )
+        
+        y_pred = booster.predict(valid)
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+        with open(models_folder / "preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact(models_folder / "preprocessor.b", artifact_path="preprocessor")
+        
+        signature = infer_signature(X_val, y_pred)
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow", signature=signature, model_format="json")
+
+        # return the run id from mlflow
+        return run.info.run_id
+        
+        
+def run(year, month):
+    df_train = read_dataframe(year, month)
+
+    next_month = month + 1 if month < 12 else 1
+    next_year = year if month < 12 else year + 1
+
+    df_val = read_dataframe(next_year, next_month)
+
+    print(f"len(df_train): {len(df_train)}, len(df_val): {len(df_val)}")
+
+    X_train, dv = create_X(df_train, None)
+    X_val, _ = create_X(df_val, dv)
+
+    target = "duration_minutes"
+    y_train, y_val = df_train[target], df_val[target]
+
+    run_id = train_model(X_train, y_train, X_val, y_val, dv)
+    return run_id
+
+if __name__ == "__main__":
+    # use argparse to get the year and month from the command line
+    parser = argparse.ArgumentParser(description="Train a model to predict the duration of a taxi trip")
+    
+    parser.add_argument("--year", type=int, default=2021, help="The year of the data to use")
+    parser.add_argument("--month", type=int, default=1, help="The month of the data to use")
+    args = parser.parse_args()
+    
+    run_id = run(year = args.year, month = args.month)
+    # save run id to a file
+    with open("run_id.txt", "w") as f:
+        f.write(run_id)
